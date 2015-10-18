33991f048441034c903faa96e42ec463adb78956
mingming <18607143517@163.com>
2015-10-18 18:31:14 +0800

    no message

---
+++ TestTwoDemo/Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by 明明 on 15/10/17.
+//  Copyright (c) 2015年 MM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestTwoDemo/Test/Test/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by 明明 on 15/10/17.
+//  Copyright (c) 2015年 MM. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.title = @"cesi";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
